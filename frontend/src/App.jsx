a25c5136039ad0698e77ff60c87a153598e03c94
Prashanth Jatavath <jatavathprashanthnaik@gmail.com>
2026-05-23 13:39:43 +0530

    Updated App routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,50 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import CreatePost from './pages/CreatePost';
-import PostDetails from './pages/PostDetails';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import PostDetails from "./pages/PostDetails";
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/create' element={<CreatePost />} />
-        <Route path='/post/:id' element={<PostDetails />} />
+
+        {/* Default Route */}
+        <Route
+          path="/"
+          element={
+            token ? <Navigate to="/home" /> : <Navigate to="/register" />
+          }
+        />
+
+        {/* Register */}
+        <Route path="/register" element={<Register />} />
+
+        {/* Login */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Home */}
+        <Route
+          path="/home"
+          element={token ? <Home /> : <Navigate to="/login" />}
+        />
+
+        {/* Create Post */}
+        <Route
+          path="/create"
+          element={token ? <CreatePost /> : <Navigate to="/login" />}
+        />
+
+        {/* Post Details */}
+        <Route
+          path="/post/:id"
+          element={token ? <PostDetails /> : <Navigate to="/login" />}
+        />
+
       </Routes>
     </BrowserRouter>
   );
