6beb55239cdd2a07bb94665e463e2166b530caca
Prashanth Jatavath <jatavathprashanthnaik@gmail.com>
2026-05-23 13:17:29 +0530

    Updated production API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://blog-platform-backend-9lg5.onrender.com/api',
 });
 
 API.interceptors.request.use((req) => {
